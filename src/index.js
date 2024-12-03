09e813985054ceeef30608ffece903b1ccee6d7a
Abøut04 <gabrielschadlick1@gmail.com>
2024-12-03 20:47:36 -0300

    Melhorias gerais

--- src/index.js
+++ src/index.js
@@ -8,12 +8,40 @@ const contentList = document.getElementById("content-list");
 
 function addContentToList(contentData) {
     const contentDiv = document.createElement("div");
-    contentDiv.innerHTML = `<div id="minecraft-content"><img src="${contentData.icon}" /></div>`;
+    contentDiv.innerHTML = `
+        <div id="content">
+            <img src="${contentData.icon}" />
+        </div>
+        <div id="content-title ">
+            <p>Dragon Evolution<p/>
+        </div>
+    `;
     contentList.append(contentDiv);
 }
 
 
 
+addContentToList({ icon: "./assets/dbe-broly.png" });
+addContentToList({ icon: "./assets/dbe-dinosaur.png" });
+addContentToList({ icon: "./assets/dbe-icon.png" });
+addContentToList({ icon: "./assets/dbe-ki-blast.png" });
+addContentToList({ icon: "./assets/dbe-namekian.png" });
+addContentToList({ icon: "./assets/dbe-saiyan.png" });
+addContentToList({ icon: "./assets/dbe-vegeta.png" });
+addContentToList({ icon: "./assets/dbe-broly.png" });
+addContentToList({ icon: "./assets/dbe-dinosaur.png" });
+addContentToList({ icon: "./assets/dbe-icon.png" });
+addContentToList({ icon: "./assets/dbe-ki-blast.png" });
+addContentToList({ icon: "./assets/dbe-namekian.png" });
+addContentToList({ icon: "./assets/dbe-saiyan.png" });
+addContentToList({ icon: "./assets/dbe-broly.png" });
+addContentToList({ icon: "./assets/dbe-dinosaur.png" });
+addContentToList({ icon: "./assets/dbe-icon.png" });
+addContentToList({ icon: "./assets/dbe-ki-blast.png" });
+addContentToList({ icon: "./assets/dbe-namekian.png" });
+addContentToList({ icon: "./assets/dbe-saiyan.png" });
+addContentToList({ icon: "./assets/dbe-vegeta.png" });
+addContentToList({ icon: "./assets/dbe-vegeta.png" });
 addContentToList({ icon: "./assets/dbe-broly.png" });
 addContentToList({ icon: "./assets/dbe-dinosaur.png" });
 addContentToList({ icon: "./assets/dbe-icon.png" });
