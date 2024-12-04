5b7d1d2536f1769cbc026140c03cfbc9f7e4fd48
Abøut04 <gabrielschadlick1@gmail.com>
2024-12-04 16:08:32 -0300

    Preparando GitHub Pages

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+const contentList = document.getElementById("content-list");
+const posts = new Map();
+
+
+
+posts.set("", {
+    title: "Patch 4.0",
+    postContent: [
+        {
+            text: "Depois de muito tempo, a nova versão do Dragon Ball Evolution (DBE) finalmente chegou! Essa atualização tão aguardada reformula completamente o addon, trazendo novas estruturas, transformações incríveis, visuais melhorados e sistemas inéditos, como combate reformulado e interações únicas com NPCs. O mundo foi renovado, com locais lendários cheios de desafios e segredos. Suas escolhas entre o bem e o mal moldarão sua jornada. Está pronto para explorar esse universo e se tornar o mais forte?"
+        },
+        {
+            image: ""
+        },
+        {
+            text: ""
+        }
+    ]
+});
